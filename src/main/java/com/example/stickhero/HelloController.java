81c2dc813fa352dbfc88abcf35723efcabbbf71f
avis <avi22119@iiitd.ac.in>
2023-11-19 21:18:44 +0530

    linked pages , updated controllers

--- src/main/java/com/example/stickhero/HelloController.java
+++ src/main/java/com/example/stickhero/HelloController.java
@@ -25,7 +25,7 @@ public class HelloController {
     }
 
     public void toGameplay(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("gameplayScreen.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("play_menu.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
